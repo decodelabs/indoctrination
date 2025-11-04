dc3ada3d6ad85e1e52916ed711ffb690c294bacc
Tom Wright <tom@inflatablecookie.com>
2025-11-04 12:44:01 +0000

    Improved doc comment headers
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Indoctrination/Config.php
+++ src/Indoctrination/Config.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @package Indoctrination
- * @license http://opensource.org/licenses/MIT
+ * Indoctrination
+ * @license https://opensource.org/licenses/MIT
  */
 
 declare(strict_types=1);
